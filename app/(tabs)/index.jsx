e0125c1cadbe521df77685903c22acb8ee8e8e5a
elmurodvokhidov <elmurodvkh@gmail.com>
2024-08-01 21:06:20 +0500

    loyiha vaqtincha to'xtatildi

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,15 +1,28 @@
 import { useEffect, useState } from 'react';
-import { ScrollView, Text, View } from 'react-native';
+import { FlatList, ScrollView, Text, TouchableOpacity, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useDispatch, useSelector } from 'react-redux';
 import { bookFailure, bookStart, bookSuccess } from '../../redux/slice/bookSlice';
 import Service from '../../config/service';
+import { categoryFailure, categoryStart, categorySuccess } from '../../redux/slice/categorySlice';
 
 export default function Home() {
     const { books } = useSelector(state => state.book);
+    const { categories } = useSelector(state => state.category);
     const dispatch = useDispatch();
     const [cat, setCat] = useState("");
     const [nomi, setNomi] = useState("");
+
+    const getAllCategories = async () => {
+        try {
+            dispatch(categoryStart());
+            const { data } = await Service.getAllCategory();
+            dispatch(categorySuccess(data));
+        } catch (error) {
+            console.log(error.message);
+            dispatch(categoryFailure(error.message));
+        }
+    };
 
     useEffect(() => {
         const getBooksFunction = async () => {
@@ -23,20 +36,30 @@ export default function Home() {
             }
         };
 
+        getAllCategories();
         getBooksFunction();
     }, [nomi, cat]);
+
+    const Item = ({ nomi }) => (
+        <View>
+            <TouchableOpacity>
+                <Text className="mx-4">{nomi}</Text>
+            </TouchableOpacity>
+        </View>
+    );
 
     return (
         <SafeAreaView>
             <ScrollView>
                 <View className="bg-blue-200 h-screen items-center justify-center">
-                    {
-                        books.map(book => (
-                            <Text key={book?._id} className="text-red-500 text-4xl">
-                                {book?.nomi}
-                            </Text>
-                        ))
-                    }
+                    <Text>Search Bar</Text>
+                    <FlatList
+                        data={categories}
+                        renderItem={({ item }) => <Item nomi={item.nomi} />}
+                        keyExtractor={item => item._id}
+                        horizontal
+                        showsHorizontalScrollIndicator
+                    />
                 </View>
             </ScrollView>
         </SafeAreaView>
